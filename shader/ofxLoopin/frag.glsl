f5f4ffec3d0bc39d279c621901a352dfdc41deaa
Samm Zuest Cooper <samm@hfour.ca>
2018-09-30 01:49:47 -0700

    hopefully make ED compatible

--- shader/ofxLoopin/frag.glsl
+++ shader/ofxLoopin/frag.glsl
@@ -1,5 +1,10 @@
-/*
-  Inputs supplied by vertex shader by loopin convention.
-*/
+#ifdef SHADER_VERSION_150
 in vec2 srcCoord;
 in vec4 vertColour;
+#endif
+#ifdef SHADER_VERSION_ES
+varying vec2 srcCoord;
+varying vec4 vertColour;
+#endif
+
+
--- shader/ofxLoopin/vert.glsl
+++ shader/ofxLoopin/vert.glsl
@@ -1,8 +1,17 @@
 uniform mat4 modelViewProjectionMatrix;
+#ifdef SHADER_VERSION_150
 in vec4 position;
 in vec2 texcoord;
 in vec4 color;
 in vec4 normal;
-
 out vec2 srcCoord;
 out vec4 vertColour;
+#endif
+#ifdef SHADER_VERSION_ES
+attribute vec4 position;
+attribute vec2 texcoord;
+attribute vec4 color;
+attribute vec4 normal;
+varying vec2 srcCoord;
+varying vec4 vertColour;
+#endif
